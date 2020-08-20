3b023a092ba170c94e4d9ff179a9ab9399c27e71
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-08-20 18:52:28 +1000

    pawn

--- engine/pieces/Pawn.py
+++ engine/pieces/Pawn.py
@@ -1,8 +1,18 @@
 from engine.pieces.PieceBase import PieceBase
 
+
 class Pawn(PieceBase):
     def __init__(self, colour):
         super().__init__(colour)
 
     def getMoves(self, useInCheck=True):
-        pass
+        direction = [1, -1][self.colour == "white"]
+        moves = []
+        if not self.board.isPiece((self.x, self.y + direction)):
+            moves.append((self.x, self.y + direction))
+        if not self.hasMoved and self.board.isPiece((self.x, self.y + direction * 2)):
+            moves.append((self.x, self.y + direction * 2))
+        for i in (1, -1):
+            if self.board.isPiece((self.x + i, self.y + direction)) and board.getPiece(
+                                  (self.x + i, self.y + direction)).colour != self.colour:
+                moves.append((self.x + i, self.y + direction))
